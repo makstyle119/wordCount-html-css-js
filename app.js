ef61f35254f0ef1f8266d0e8c24a064c5486ff99
makstyle119 <moizprince786@gmail.com>
2023-03-26 00:48:40 +0500

    add character count

--- app.js
+++ app.js
@@ -13,9 +13,23 @@ const countWords = () => {
   outputDiv.textContent = `Number of words: ${numWords}`;
 };
 
+const charWords = () => {
+  // Get the input value
+  const inputText = document.getElementById('text-input').value;
+
+  // Count the number of character
+  const charCount = inputText.length;
+
+  // Display the character count in the output div
+  const outputDiv = document.getElementById('output');
+  outputDiv.textContent = `Number of Characters: ${charCount}`;
+};
+
 // Add an event listener to the count button
 const countBtn = document.getElementById('count-btn');
+const charBtn = document.getElementById('char-btn');
 countBtn.addEventListener('click', countWords);
+charBtn.addEventListener('click', charWords);
 
 // get the current year
 const getYearAndUrl = () => {
